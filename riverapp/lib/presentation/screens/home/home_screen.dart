bc703c89ff686e2dcaea37c3c122f32d6d48dde2
Edwin bikes <edwinbykes@hotmail.com>
2023-11-30 15:56:34 -0500

    homescreen with provider ands routers

--- riverapp/lib/presentation/screens/home/home_screen.dart
+++ riverapp/lib/presentation/screens/home/home_screen.dart
@@ -23,58 +23,7 @@ class _HomeScreenView extends StatelessWidget {
     return const CustomScrollView(
       slivers: [
         HomeWidgets(),
-        // _CustomListTile(
-        //   title: "Nombres Aleatorios",
-        //   subTitle: "State provider",
-        //   location: "/state-provider",
-        // ),
-        // _CustomListTile(
-        //   title: "Nombres Pokemons API",
-        //   subTitle: "Future Provider",
-        //   location: "/future-provider",
-        // ),
-        // _CustomListTile(
-        //   title: "Nombres de personas Random",
-        //   subTitle: "Stream provider",
-        //   location: "/stream-provider",
-        // ),
-        // _CustomListTile(
-        //   title: "State notifier Provider",
-        //   subTitle: "State provider personalizado",
-        //   location: "/state-notifier-provider",
-        // ),
-        // _CustomListTile(
-        //   title: "Permisos de la aplicacion",
-        //   subTitle: "Solicitar permisos (cámara),(gps),(bluetooth)...",
-        //   location: "/permissions-screen",
-        // ),
-        // ListTile(
-        //   leading: Icon(Icons.web_stories_outlined),
-        //   title: Text('Edwin Bikes',
-        //       style: TextStyle(fontSize: 14, color: Colors.black)),
-        //   subtitle: Text('Sigueme en mis Redes',
-        //       style: TextStyle(fontSize: 12, color: Colors.grey)),
-        //   trailing: Icon(Icons.arrow_forward_ios_rounded),
-        // )
       ],
     );
   }
 }
-
-// class _CustomListTile extends StatelessWidget {
-//   final String title;
-//   final String subTitle;
-//   final String location;
-//   const _CustomListTile(
-//       {required this.title, required this.subTitle, required this.location});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListTile(
-//       title: Text(title),
-//       subtitle: Text(subTitle),
-//       trailing: const Icon(Icons.arrow_forward_ios_rounded),
-//       onTap: () => context.push(location),
-//     );
-//   }
-// }
